d0c1e64c42a8cba42c07d4f748affd2b6fc1fee0
sevilsariyeva <sevlsariyeva00@gmail.com>
2025-04-15 11:15:56 +0400

    NSwag is added

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -13,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApiDocument();
 
 var connection = builder.Configuration.GetConnectionString("myconn");
 builder.Services.AddDbContext<MarketDbContext>(opt =>
@@ -29,10 +29,11 @@ builder.Services.AddScoped<ICustomerService, CustomerService>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 
+
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
+    app.UseOpenApi();
     app.UseSwaggerUI();
 }
 
